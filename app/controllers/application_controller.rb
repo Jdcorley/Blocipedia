4cc1b00524ae0c6214b78ceed36f441cfd486603
Jack <jdcorley@gmail.com>
2018-03-09 09:49:49 -0500

    devise sign-up w/ username complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-  protect_from_forgery with: :exception
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller? 
 
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
-      user_params.permit(:username, :email)
-    end
+  protected 
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ApplicationRecord
+  attr_accessor :login 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
+         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
+         
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    validates :username, presence: :true, uniqueness: { case_sensitive: false }
+    validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+     end
+   end
+  end
+end 
