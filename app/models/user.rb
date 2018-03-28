505f3f44ecbdc1cf422732cb7053f136bccde871
Jack <jdcorley@gmail.com>
2018-03-28 15:48:05 -0400

    make private wikis public for standard acccount callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,31 +1,41 @@
 class User < ApplicationRecord
-  has_many :wikis, dependent: :destroy 
-  attr_accessor :login
+  has_many :wikis, dependent: :destroy
 
-  enum role: [:standard, :premium, :admin]
+  after_save :check_role
   after_initialize :set_default_role, :if => :new_record?
+  
+  enum role: [:standard, :premium, :admin]
+
+  devise :database_authenticatable, :registerable, :confirmable,
+  :recoverable, :rememberable, :trackable, :validatable, 
+  authentication_keys: [:login]
+
+  attr_accessor :login
 
   def set_default_role
     self.role ||= :standard
-  end
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable, authentication_keys: [:login]
-         
+  end 
+
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     validates :username, presence: :true, uniqueness: { case_sensitive: false }
     validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
-
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-    else
-      if conditions[:username].nil?
+        where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+      elsif conditions[:username].nil?
         where(conditions).first
       else
         where(username: conditions[:username]).first
-     end
    end
-  end
-end 
+ end
+
+private 
+
+  def check_role 
+    wikis = self.wikis 
+    if self.standard?
+      wikis.update_all private: false 
+      puts wikis 
+    end 
+  end 
+end
